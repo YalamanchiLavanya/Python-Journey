89d7c5bc985ae8f55ded1a4e670f3b655d6d46af
YalamanchiLavanya <lavanyayalamanchi2005@gmail.com>
2026-06-22 02:37:20 -0700

    Add files via upload

---
+++ Notes1.py
@@ -0,0 +1,78 @@
+'''Procedural Language:
+   -------------------
+--->This follows a step-by-step approach , where code is structure int procedures such as function or routines
+Ex: C Language
+
+
+Object Oriented Language:
+------------------------
+--->This is based on concepts of Object and Class
+EX: Python ,java
+ex:
+   print(89+67)
+   num=89
+   num_2=67
+   total_=num+num_2
+   print(id(num))
+   print(id(num_2))
+   print(id(total-))
+ 
+---> What is Python?  
+     1.Dynamically Typed Language-->Python  knows the type of data we are passing to the variable
+     2.Interpreter-->Python Execute code Line-by-Line if any error it will be stop the execution int and before will gives the output
+      EX:
+         num=89
+         print(type(num))
+         num_2=67
+         total_=num+num_2
+         print(total)
+         Print(id(total_))
+     3.Highlevel
+**1991-->Guide Van Rossum
+
+--->Why Using Python?
+*Easy Syntax
+*Cross Platform
+*Wide Applications
+*Huge Libary Support
+
+Tokens:
+1.Key words--> there are reserved words in python
+EX: if else for while
+2.Identifires-->Names given to variables , functions , classes
+3.Literals--->89, "hello",4.56
+4.Operators--->+,-,*....
+5.Punctuators--->(),{},[]
+
+
+Rules of Variables:
+
+---> A-Z, a-z
+ex: num-1=7
+    num=8
+    Num_2=9
+--->Special Char, Number, Space are not be in first position
+ex: 2num=9
+    #num=0
+    num 2_=2
+    
+
+Comments:
+1.Single Line-->#
+2.Multi Line-->''' ''',""" """
+
+num=9
+if num%2==0:#
+  print("even")
+else
+print("odd")
+
+Boolean:
+--->True
+--->False
+num=8
+num_2=8
+print(num==num_2)
+
+'''
+ 
