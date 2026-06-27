625ca2f6d2cd86e17f5672ee8b79e5b604e985bf
YalamanchiLavanya <lavanyayalamanchi2005@gmail.com>
2026-06-27 05:02:24 -0700

    Add files via upload
    This repository provides a comprehensive overview of Python operators with simple explanations and practical examples. It covers Arithmetic Operators, Comparison Operators, Logical Operators, Assignment Operators, Bitwise Operators, Membership Operators, and Identity Operators, it a useful resource for beginners Python programming fundamentals. 🚀

---
+++ Day2-Operators.py
@@ -0,0 +1,106 @@
+'''
+Operators:
+
+1.Arithmetic Operator
+---------------------
+-->+,-,*,%,**,//,/
+#Arithmetic Operators Example
+a=3
+b=9
+print(a+b)
+print(b-a)
+print(a*b)
+print(a%b)
+print(a**b)
+print(a/b)
+print(a//b)
+
+
+2.Assignment Operator
+---------------------
+--->=,+=,-=,*=,%=,
+#Assignment Operator Example
+a=20
+b=10
+print(a)
+print(b)
+a+=4
+print(a)
+b-=2
+print(b)
+a*=4
+print(a)
+b%=2
+print(b)
+
+
+3.Comparision Operator
+----------------------
+--->==,!=,<,>,<=,>=
+#Comparision Operator Example
+a=20
+b=10
+print(a==b)
+print(a<b)
+print(b>a)
+print(a<=b)
+print(b>=a)
+print(a!=b)
+
+4.Logical Operator
+------------------
+--->and: Return True if both are true
+--->or: Return true if any one is correct
+--->not: Reverse the Output
+#Logical Operator Example
+a=20
+b=7
+c=89
+print(a>=b and b<=c)
+print(a>=b or b<=c)
+print(not(a>=b and b<=c))
+
+
+5.Identity Operator
+-------------------
+1.is-->Same object or not(Checks Memory address) prints true
+2.is not-->not same Object
+
+#Identity Operator Example
+a=20
+b=20
+print(a==b)
+print(a is b)
+a=[1,2]
+b=[1,2]
+print(a==b)
+print(a is b)
+print(a is not b)
+
+
+6.Membership Operator
+---------------------
+1.in-->is there or not
+2.not in--> is not there or there
+#Membership Operator Example
+a=[1,2,3]
+print(2 in a)
+b="Hello"
+print("a" in b)
+print (20 not in a)
+
+
+
+7.Bitwise Operator
+------------------
+---> &,<<,>>,|,^
+#Bitwise Operator Example
+a=5
+b=3
+print(a & b)
+print(a << b)
+print(a >> b)
+print(a | b)
+print(a ^ b)
+
+'''
