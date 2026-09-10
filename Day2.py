e2f3a9c5656bc2c17ae6e018752831de3e4ebd6d
YalamanchiLavanya <lavanyayalamanchi2005@gmail.com>
2026-09-10 07:15:23 +0530

    Recap of Control Flow Statements

--- Day2.py
+++ Day2.py
@@ -49,47 +49,32 @@ for i in range(number_of_times_user_input):
 # same above task we need to handle the erros(Expection handling) and also
 #Make user strictly to enter only numeric values
 '''
-results = []
-number_of_users = int(input("Enter the number of users: "))
-for i in range(number_of_users):
-    while True:
-        try:
-            name = input("Enter your name: ")
-            weight = float(input("Enter the weight in Kgs: "))
-            height = float(input("Enter the height in meters: "))
 
-            if weight > 0 and height > 0:
-                break
-            else:
-                print("Enter only positive values")
+while True:
+    try:
+        name = input("Enter your name: ")
+        weight = float(input("Enter the weight in Kgs: "))
+        height = float(input("Enter the height in meters: "))
 
-        except ValueError:
-            print("Enter only numeric values for weight and height")
+        if weight > 0 and height > 0:
+            break
+        else:
+            print("Enter only positive values")
 
-    bmi = weight / (height ** 2)
+    except ValueError:
+        print("Enter only numeric values for weight and height")
+bmi = weight / (height ** 2)
 
-    if bmi<18.5:
-        print(f'{name} is into Under Weight category and BMI is {bmi}')
-    elif  18.5<=bmi<=24.9:
-        print(f'{name} is into Normal Weight category and BMI is {bmi}')
-    elif 25<=bmi<=29.9:
-        print(f'{name} is into Over Weight category and BMI is {bmi}')
-    elif bmi>=30:
-        print(f'{name} is into Obesity category and BMI is {bmi}')
+if bmi<18.5:
+    print(f'{name} is into Under Weight category and BMI is {bmi}')
+elif  18.5<=bmi<=24.9:
+    print(f'{name} is into Normal Weight category and BMI is {bmi}')
+elif 25<=bmi<=29.9:
+    print(f'{name} is into Over Weight category and BMI is {bmi}')
+elif bmi>=30:
+    print(f'{name} is into Obesity category and BMI is {bmi}')
 
-    result = {
-        "name": name,
-        "weight": weight,
-        "height": height,
-        "BMI": bmi,
-    }
-
-    results.update(result)
-
-print("\nBMI Results:")
-
-for result in results:
-    print(result)
+  
 
 
 
